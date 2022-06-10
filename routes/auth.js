a05361037ef5c817a5183f883a911d18b6a08af5
blackbox <dzzirima@gmail.com>
2022-06-11 00:10:47 +0200

    feat :signin user

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 import express from "express";
 import User from "../model/user.js";
-import bcrypt from "bcryptjs/dist/bcrypt.js";
+import bcrypt from "bcryptjs";
+import  Jwt  from "jsonwebtoken";
 
 const authRoute = express.Router();
 
@@ -9,7 +10,7 @@ authRoute.post("/api/signup", async (req, res) => {
     //get the data from the user
     const { name, email, password } = req.body;
 
-    const existingUSer = await User.findOne({email});
+    const existingUSer = await User.findOne({ email });
 
     if (existingUSer) {
       return res
@@ -28,7 +29,37 @@ authRoute.post("/api/signup", async (req, res) => {
     return res.json(user);
     //return to the user
   } catch (error) {
-      return res.status(500).json({msg:error.message})
+    return res.status(500).json({ msg: error.message });
+  }
+});
+
+//sign in User
+// jwt will allow us to verify who we say are we
+authRoute.post("/api/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    //gard closes
+    if (!user) {
+      return res.status(400).json({msg:"User with this email does not exist !"})
+    }
+
+    //due to the salt issues 2 paswords will never have the same hashed passwords
+    const isMatch = await bcrypt.compare(password,user.password)
+
+    if(!isMatch){
+      return res.status(400).json({msg:"Incorrect Password"})
+    }
+
+    //token should be stored in memory
+    const token = Jwt.sign({id:user._id},"passwordKey")
+
+    return res.json({token,...user._doc})
+
+
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
   }
 });
 
